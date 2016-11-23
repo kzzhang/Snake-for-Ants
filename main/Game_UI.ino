0aa4e11f93130c6c875e2ab3ac7726fed062328d
Robert Xu <RobertYC.Xu@gmail.com>
2016-11-23 05:34:48 +0000

    Added src files, main, gui, wire util

---
+++ main/Game_UI.ino
@@ -0,0 +1,99 @@
+#include <stdbool.h>
+#include <string.h>
+
+static enum GamePages
+{
+  Welcome       = 0,
+  Snake         = 1,
+  Score         = 2,
+  NumberOfPages = 3,
+} gameUiPage = Welcome;
+
+const uint32_t ButtonCount = 2;
+
+const uint32_t Buttons[ButtonCount] = { PD_2, PE_0 };
+
+struct ButtonState
+{ 
+  bool state;
+  bool isRising;
+};
+
+static struct InputState
+{
+  struct ButtonState  buttons[2];
+} gameInputState;
+
+void GameUIInit()
+{
+  OrbitOledInit();
+  OrbitOledClear();
+  OrbitOledClearBuffer();
+  OrbitOledSetFillPattern(OrbitOledGetStdPattern(iptnSolid));
+  DelayInit();
+  OrbitOledSetDrawMode(modOledSet);
+
+  
+
+  for(int i = 0; i < ButtonCount; ++i )
+    pinMode(Buttons[i], INPUT);
+}
+
+static void uiInputTick()
+{
+  
+  for(int i = 0; i < ButtonCount; ++i )
+  {
+    // Only look for Rising Edge Signals.
+    bool previousState = gameInputState.buttons[i].state;
+    gameInputState.buttons[i].state = digitalRead(Buttons[i]);
+    gameInputState.buttons[i].isRising = (!previousState && gameInputState.buttons[i].state);
+  }
+ 
+}
+
+
+static void handlePageWelcome()
+{
+  OrbitOledMoveTo(5, 0);
+  OrbitOledDrawString("Welcome to SFA!");
+  
+  OrbitOledMoveTo(10, 15);
+  OrbitOledDrawString("Press BTN1");
+DelayMs(1000);
+  OrbitOledMoveTo(27, 27);
+  OrbitOledLineTo(31,27);
+  OrbitOledUpdate();
+  DelayMs(1000);
+  OrbitOledLineTo(32,27);
+  if(gameInputState.buttons[1].isRising)
+  {
+    OrbitOledClearBuffer();
+    OrbitOledClear();
+    gameUiPage = Snake;
+  }
+
+  
+}
+
+static void handlePageSnake(){
+  OrbitOledMoveTo(5, 0);
+  OrbitOledDrawString("snake interface!");
+}
+
+
+void GameUITick()
+{
+  uiInputTick();
+  switch(gameUiPage)
+  {
+  case Welcome:
+    handlePageWelcome();
+    break;
+  case Snake:
+    handlePageSnake();
+    break;
+  
+  }
+  OrbitOledUpdate();
+}
---
+++ main/main.ino
@@ -0,0 +1,32 @@
+#include "src/FillPat.h"
+#include "src/LaunchPad.h"
+#include "src/OrbitBoosterPackDefs.h"
+#include "src/OrbitOled.h"
+#include "src/OrbitOledChar.h"
+#include "src/OrbitOledGrph.h"
+#include "src/delay.h"
+
+#include <stdlib.h>
+
+void WireInit();
+void GameUIInit();
+
+void GameUITick();
+
+
+
+void setup() 
+{
+  WireInit();
+  Serial.begin(9600);
+  delay(100);
+  GameUIInit();
+
+  
+}
+
+void loop() 
+{
+  
+  GameUITick();
+}
